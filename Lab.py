5f04b234373ec615a5c55d73339a5f81585d2628
marcelop87 <126330395+marcelop87@users.noreply.github.com>
2023-04-30 02:47:06 -0400

    Update Lab.py

--- Lab.py
+++ Lab.py
@@ -24,7 +24,7 @@ def load_data(file):
   df= df.replace({'\-' : np.nan , '\*' : np.nan, '^\s*$': np.nan }, regex=True)
   df.reset_index(inplace=True, level='Pozo')
   df.dropna(subset='Fecha', inplace=True)
-  df.Fecha = df.Fecha.astype("datetime64[ns]")
+  df.Fecha = df.Fecha.dt.date
   return df
 
 
