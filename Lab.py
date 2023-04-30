bff6c1de74c961da2fa5caef449278e1c06d49b4
marcelop87 <126330395+marcelop87@users.noreply.github.com>
2023-04-30 19:57:04 -0400

    Update Lab.py

--- Lab.py
+++ Lab.py
@@ -34,7 +34,7 @@ def sort_data(df):
   # Sort Data
   sort_column = st.sidebar.selectbox("Sort by", df.columns)
   df = df.sort_values(by=sort_column)
-  df = df.fillna(value=np. nan)
+  df.fillna(value=pd.np.nan, inplace=True)
   df.reset_index(inplace=True, drop=True)
   return df
 
