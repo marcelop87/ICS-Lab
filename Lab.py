e76a9e0aab5cdeab4cb2c7288d78881fb88b6040
marcelop87 <126330395+marcelop87@users.noreply.github.com>
2023-05-01 04:22:44 -0400

    Update Lab.py

--- Lab.py
+++ Lab.py
@@ -34,7 +34,7 @@ def sort_data(df):
 
   # Sort Data
   sort_column = st.sidebar.selectbox("Sort by", df.columns)
-  df = df.sort_values(by=sort_column)
+  df = df.sort_values(by=[sort_column, 'Fecha'])
   df.reset_index(inplace=True, drop=True)
   return df
 
